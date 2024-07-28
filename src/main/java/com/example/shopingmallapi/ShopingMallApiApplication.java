156e27044dac03ff5e937986b156546bb020da42
wisekim <lwisekiml@naver.com>
2024-07-28 18:14:22 +0900

    first commit

---
+++ src/main/java/com/example/shopingmallapi/ShopingMallApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopingmallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingMallApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingMallApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shopingmallapi/ShopingMallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopingmallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingMallApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
