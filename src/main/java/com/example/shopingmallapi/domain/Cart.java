83561d3e491e773ff7f17e999f3a6d0f4b45d131
wisekim <lwisekiml@naver.com>
2024-08-04 14:44:12 +0900

    Cart, CartItem

---
+++ src/main/java/com/example/shopingmallapi/domain/Cart.java
@@ -0,0 +1,25 @@
+package com.example.shopingmallapi.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
+@Getter @Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long memberId;
+
+    private String date; // yyyymmdd
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItems = new ArrayList<>();
+}
