180c7febda294a1723677216d1b01ba01932782b
wisekim <lwisekiml@naver.com>
2024-08-05 18:46:06 +0900

    프론트엔트 변경

--- src/main/java/com/example/shopingmallapi/frontend/pages/index.js
+++ src/main/java/com/example/shopingmallapi/frontend/pages/index.js
@@ -1,54 +1,11 @@
-import Link from "next/link";
+import React from "react";
 
-function Home() {
-  const handleLogout = async () => {
-    try {
-      const loginData = localStorage.getItem("loginData");
-      if (!loginData) {
-        console.error("No login data found in local storage.");
-        return;
-      }
-
-      const { accessToken, refreshToken } = JSON.parse(loginData);
-      if (!refreshToken) {
-        console.error("No refresh token found in login data.");
-        return;
-      }
-
-      const response = await fetch("http://localhost:8080/members/logout", {
-        method: "DELETE",  // DELETE 대신 POST로 변경
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          refreshToken: refreshToken,
-        }),
-      });
-
-      if (response.ok) {
-        localStorage.removeItem("loginData");
-      } else {
-        console.error(response.statusText);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+const HomePage = () => {
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
-      <Link href="/about">
-        <div>About</div>
-      </Link>
-      <br />
-      <Link href="/login">
-        <div>Login</div>
-      </Link>
-      <button onClick={handleLogout}>Logout</button>
+      <h1>홈페이지</h1>
     </div>
   );
-}
+};
 
-export default Home;
+export default HomePage;
--- src/main/java/com/example/shopingmallapi/frontend/pages/login.js
+++ src/main/java/com/example/shopingmallapi/frontend/pages/login.js
@@ -1,57 +1,113 @@
+import React from "react";
+import { Container, Box, Typography, TextField, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Link from "next/link";
+import axios from "axios";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    maxWidth: "500px",
+    height: "100%",
+    margin: "0 auto",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+  },
+  backButton: {
+    alignSelf: "flex-start",
+  },
+}));
+
+const Login = () => {
+  const classes = useStyles();
   const router = useRouter();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await fetch("http://localhost:8080/members/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await response.json();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
-      if (response.ok) {
-        console.log(data);
-        localStorage.setItem("loginData", JSON.stringify(data));
-        router.push("/loginWelcome");
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:8080/members/login", {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        const loginInfo = response.data;
+        localStorage.setItem("loginInfo", JSON.stringify(loginInfo));
+        router.push("/");
+
+        // 로그인 상태 변경 이벤트 발생
+        const event = new Event("loginStatusChanged");
+        window.dispatchEvent(event);
       }
     } catch (error) {
       console.error(error);
+      setErrorMessage("이메일이나 암호가 틀렸습니다.");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <label htmlFor="email">
-        Email:
-        <input
+    <Container maxWidth="sm" className={classes.container} component="main">
+      {/* 변경된 코드 */}
+      <Typography variant="h4" component="h1" gutterBottom>
+        로그인
+      </Typography>
+      <Box component="form" className={classes.form} onSubmit={handleLogin}>
+        <TextField
+          label="이메일"
           type="email"
-          id="email"
+          variant="outlined"
+          margin="normal"
+          fullWidth
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-      </label>
-      <br />
-      <label htmlFor="password">
-        Password:
-        <input
+        <TextField
+          label="비밀번호"
           type="password"
-          id="password"
+          variant="outlined"
+          margin="normal"
+          fullWidth
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+        {errorMessage && (
+          <Typography variant="body1" color="error" paragraph>
+            {errorMessage}
+          </Typography>
+        )}
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth
+        >
+          로그인
+        </Button>
+
+        <Link href="/joinform" passHref>
+          <Button color="inherit">회원가입</Button>
+        </Link>
+        <Link href="/findpassword" passHref>
+          <Button color="inherit">암호를 잊었어요.</Button>
+        </Link>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default Login;
