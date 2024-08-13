f3ac3af2a172c3c142236039245324066a24fe7c
wisekim <lwisekiml@naver.com>
2024-08-13 18:36:21 +0900

    게시글 삭제

--- springboot_board_project/src/main/java/com/example/springboot_board_project/controller/BoardController.java
+++ springboot_board_project/src/main/java/com/example/springboot_board_project/controller/BoardController.java
@@ -57,4 +57,10 @@ public class BoardController {
         return "detail";
 //        return "redirect:/board/" + boardDTO.getId();
     }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
 }
